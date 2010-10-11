2e348ae6c72a53844ba5fa504d08d848953d6760
Walter Lee Davis <waltd@wdstudio.com>
2010-10-11 12:23:48 -0400

    removed generated code from example

--- generated_code/test/_views/titles/show.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-	<p><strong>name</strong><br />
-		<?= $object->h('name') ?>
-	</p>
-	<p><strong>description</strong></p>
-	<?= ActionView::simple_format('description',$object) ?>
-	<?= '<p>' . ActionView::link_to("Index","index",$object) . ' | ' . ActionView::link_to("Edit","edit",$object) . '</p>' ?>
